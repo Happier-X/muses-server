48c904b1c2137287ab2a6bdeca6d4a49c1ee3219
赵鸿飞 <zhf5256@qq.com>
2025-08-14 15:12:09 +0800

    fix: 修复token问题

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,29 +1,40 @@
-import { Body, Controller, Get, Post, Query, Req, UseGuards } from '@nestjs/common';
-import { Request } from 'express';
-import { AuthService } from './auth.service';
-import { Public } from './decorator/auth.decorator';
-import { RegisterDto } from './dto/register.dto';
-import { LocalAuthGuard } from './guard/local-auth.guard';
+import {
+    Body,
+    Controller,
+    Get,
+    Post,
+    Query,
+    Req,
+    UseGuards
+} from '@nestjs/common'
+import { Request } from 'express'
+import { AuthService } from './auth.service'
+import { Public } from './decorator/auth.decorator'
+import { RegisterDto } from './dto/register.dto'
+import { LocalAuthGuard } from './guard/local-auth.guard'
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) { }
+    constructor(private readonly authService: AuthService) {}
 
     @Public()
     @UseGuards(LocalAuthGuard)
     @Post('login')
     login(@Req() req: Request) {
-        return this.authService.login(req.user);
+        return this.authService.login(req.user)
     }
 
     @Public()
     @Post('register')
     register(@Body() registerObj: RegisterDto) {
-        return this.authService.register(registerObj);
+        return this.authService.register(registerObj)
     }
 
+    @Public()
     @Get('refreshToken')
     refreshToken(@Req() req: Request) {
-        return this.authService.refreshToken(req.headers.refreshToken as string);
+        return this.authService.refreshToken(
+            req.headers['refresh-token'] as string
+        )
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,68 +1,68 @@
-import { HttpStatus, Injectable } from '@nestjs/common';
-import { UsersService } from 'src/users/users.service';
-import * as argon2 from 'argon2';
-import { RegisterDto } from './dto/register.dto';
-import { JwtService } from '@nestjs/jwt';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { HttpStatus, Injectable } from '@nestjs/common'
+import { UsersService } from 'src/users/users.service'
+import * as argon2 from 'argon2'
+import { RegisterDto } from './dto/register.dto'
+import { JwtService } from '@nestjs/jwt'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private usersService: UsersService,
-    private jwtService: JwtService,
-    private prisma: PrismaService,
-  ) {}
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService,
+        private prisma: PrismaService
+    ) {}
 
-  async register(registerObj: RegisterDto) {
-    const password = await argon2.hash(registerObj.password);
-    return await this.usersService.create(registerObj.username, password);
-  }
-
-  async validateUser(username: string, password: string): Promise<any> {
-    const user = await this.usersService.findOne(username);
-    if (!user) {
-      return null;
+    async register(registerObj: RegisterDto) {
+        const password = await argon2.hash(registerObj.password)
+        return await this.usersService.create(registerObj.username, password)
     }
-    const isPasswordValid = await argon2.verify(user.password, password);
-    if (!isPasswordValid) {
-      return null;
-    } else {
-      const { password, ...result } = user;
-      return result;
-    }
-  }
 
-  async login(user: any) {
-    const payload = { username: user.username, sub: user.id };
-    return {
-      statusCode: HttpStatus.CREATED,
-      access_token: this.jwtService.sign(payload),
-      refresh_token: this.jwtService.sign(payload, {
-        expiresIn: '14d',
-      }),
-    };
-  }
-
-  async refreshToken(refreshToken: string) {
-    const { sub: userId } = this.jwtService.verify(refreshToken);
-    const user = await this.prisma.user.findUnique({
-      where: {
-        id: userId,
-      },
-    });
-    if (user) {
-      const payload = { username: user.username, sub: user.id };
-      return {
-        statusCode: HttpStatus.CREATED,
-        access_token: this.jwtService.sign(payload),
-        refresh_token: this.jwtService.sign(payload, {
-          expiresIn: '14d',
-        }),
-      };
+    async validateUser(username: string, password: string): Promise<any> {
+        const user = await this.usersService.findOne(username)
+        if (!user) {
+            return null
+        }
+        const isPasswordValid = await argon2.verify(user.password, password)
+        if (!isPasswordValid) {
+            return null
+        } else {
+            const { password, ...result } = user
+            return result
+        }
     }
-    return {
-      statusCode: HttpStatus.UNAUTHORIZED,
-      message: 'refreshToken失效，请重新登录',
-    };
-  }
+
+    async login(user: any) {
+        const payload = { username: user.username, sub: user.id }
+        return {
+            statusCode: HttpStatus.CREATED,
+            access_token: this.jwtService.sign(payload),
+            refresh_token: this.jwtService.sign(payload, {
+                expiresIn: '14d'
+            })
+        }
+    }
+
+    async refreshToken(refreshToken: string) {
+        const { sub: userId } = this.jwtService.verify(refreshToken)
+        const user = await this.prisma.user.findUnique({
+            where: {
+                id: userId
+            }
+        })
+        if (user) {
+            const payload = { username: user.username, sub: user.id }
+            return {
+                statusCode: HttpStatus.CREATED,
+                access_token: this.jwtService.sign(payload),
+                refresh_token: this.jwtService.sign(payload, {
+                    expiresIn: '14d'
+                })
+            }
+        }
+        return {
+            statusCode: HttpStatus.UNAUTHORIZED,
+            message: 'refreshToken失效，请重新登录'
+        }
+    }
 }
