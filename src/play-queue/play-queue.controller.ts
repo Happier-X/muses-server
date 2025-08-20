c7b65d10a07421c8981119ed5268ca1e5bd01574
赵鸿飞 <zhf5256@qq.com>
2025-08-20 16:51:30 +0800

    feat: 完成播放列表查询

--- src/play-queue/play-queue.controller.ts
+++ src/play-queue/play-queue.controller.ts
@@ -1,9 +1,21 @@
-import { Body, Controller, Post, Body } from '@nestjs/common'
+import { Body, Controller, Get, Post, Req } from '@nestjs/common'
+import { Request } from 'express'
+import { PlayQueueService } from './play-queue.service'
 
 @Controller('play-queue')
 export class PlayQueueController {
+    constructor(private readonly playQueueService: PlayQueueService) {}
+
     @Post('add')
-    addToPlayQueue(@Body('id') id: string) {
-        return 'addToPlayQueue'
+    addToPlayQueue(
+        @Body('songIdList') songIdList: string[],
+        @Req() req: Request
+    ) {
+        return this.playQueueService.addToPlayQueue(songIdList, req.user)
+    }
+
+    @Get('get')
+    getPlayQueue(@Req() req: Request) {
+        return this.playQueueService.getPlayQueue(req.user)
     }
 }
--- src/play-queue/play-queue.service.ts
+++ src/play-queue/play-queue.service.ts
@@ -1,4 +1,54 @@
 import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 @Injectable()
-export class PlayQueueService {}
+export class PlayQueueService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async addToPlayQueue(songIdList: string[], user: any) {
+        const { id: userId } = user
+        const maxPositionItem = await this.prisma.queueItem.findFirst({
+            where: { userId },
+            orderBy: { position: 'desc' }
+        })
+        let nextPosition = maxPositionItem ? maxPositionItem.position + 1 : 0
+        for (let item of songIdList) {
+            const existingItem = await this.prisma.queueItem.findUnique({
+                where: {
+                    userId_songId: {
+                        userId,
+                        songId: parseInt(item)
+                    }
+                }
+            })
+            if (!existingItem) {
+                await this.prisma.queueItem.create({
+                    data: {
+                        userId,
+                        songId: parseInt(item),
+                        position: nextPosition++
+                    }
+                })
+            }
+        }
+    }
+
+    async getPlayQueue(user: any) {
+        const { id: userId } = user
+        return this.prisma.queueItem.findMany({
+            where: { userId },
+            orderBy: { position: 'asc' },
+            include: {
+                song: {
+                    select: {
+                        title: true,
+                        artist: true,
+                        album: true,
+                        cover: true,
+                        duration: true
+                    }
+                }
+            }
+        })
+    }
+}
